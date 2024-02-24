7b328848c0766683901987f822d34d7aac1b7fb0
inyoung <rmdnps10@gmail.com>
2024-02-24 20:19:45 +0900

    Feat: ChatInput 줄의 수가 넘어감에 따라 height 반응형으로

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import sendIcon from '../assets/images/send-icon.svg';
+import TextareaAutosize from 'react-textarea-autosize';
+import styled from 'styled-components';
 function ChatInput() {
   return (
-    <div className="w-[100%] h-[66px] p-5 border-t border-neutral-300 flex-col justify-start items-center gap-[30px] inline-flex fixed bottom-0 left-0">
-      <div className="w-[400px] h-[66px] mx-auto bg-neutral-50 p-20">
+    <div className="w-[100%] p-5 pb-[20px] border-t border-neutral-300 flex-col justify-start items-center gap-[30px] inline-flex fixed bottom-0 left-0">
+      <div className="w-[320px] h-[66px] mx-auto bg-neutral-50 p-20">
         <div className="pl-px pt-px pb-[5px] justify-center items-start w-[100%] inline-flex">
-          <input
+          <MessageTextArea
             className="text-neutral-500 text-[15px] leading-tight inline-block w-[100%]"
             placeholder={'무엇이든 물어보세요...'}
+            rows={1}
+            maxRows={4}
           />
           <img src={sendIcon} alt="보내기 버튼" />
         </div>
@@ -17,14 +21,14 @@ function ChatInput() {
 }
 const MessageTextArea = styled(TextareaAutosize)`
   width: 100%;
-  padding: 1.2rem 1.5rem;
   outline: none;
   border: none;
   resize: none;
   border-radius: 1.2rem;
-  background-color: ${Grey6};
-  font-family: Pretendard;
   font-size: 1.6rem;
+  color: black;
+  overflow: visible;
+  background-color: rgba(250, 250, 250, 1);
   font-style: normal;
   font-weight: 400;
   line-height: 110%; /* 1.76rem */
